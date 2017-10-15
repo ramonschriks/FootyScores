2da9c600380b258e34d7b2bf3623ebc9d51adb04
Ramon Schriks <ramon@xel.nl>
2017-10-16 00:14:35 +0200

    Initial project setup

---
+++ FootyScores/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  FootyScores
+//
+//  Created by Ramon Schriks on 16-10-17.
+//  Copyright © 2017 Ramon Schriks. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Podfile
@@ -0,0 +1,7 @@
+use_frameworks!
+
+target 'FootyScores' do
+	pod 'Alamofire', '~> 4.0'
+	pod 'SwiftyJSON’, :git => 'https://github.com/acegreen/SwiftyJSON.git', :branch => 'swift3'
+end
+
