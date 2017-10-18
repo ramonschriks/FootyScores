cbfa2673a32e67c681b395089da17f2583402cec
Ramon Schriks <ramon@xel.nl>
2017-10-18 10:04:26 +0200

    Added sorting on country Id for each event

--- FootyScores/Services/EventService.swift
+++ FootyScores/Services/EventService.swift
@@ -17,8 +17,28 @@ class EventService {
         formatter.dateFormat = "yyyy.MM.dd"
         let today = formatter.string(from: date)
         
-        self.client.getEvents(fromDate: today, toDate: today) { events in
-            // TODO: Handle events
+        self.client.getEvents(fromDate: today, toDate: today) { [weak weakSelf = self] events in
+            var sortedEvents = weakSelf?.sortEventsOnCountry(events)
         }
+    }
+    
+    /**
+     * Sort the events based on the on the country ID (lower to country key, the important the league).
+     * Create a new Dictionary from the events that hold the country id as key.
+     */
+    private func sortEventsOnCountry(_ events: [Event]) -> [(key: String, value: [Event])] {
+        var countryEvents: [String: [Event]] = [:]
+        for event in events {
+            if countryEvents[event.country_id] == nil {
+                countryEvents[event.country_id] = []
+            }
+            countryEvents[event.country_id]?.append(event)
+        }
+        
+        let sortedEvents = countryEvents.sorted { (first: (key: String, value: [Event]), second: (key: String, value: [Event])) -> Bool in
+            return first.key < second.key
+        }
+   
+        return sortedEvents
     }
 }
